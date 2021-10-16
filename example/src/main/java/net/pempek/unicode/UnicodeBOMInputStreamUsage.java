e9b8e4ba3a9af766c08702794336d9419fcc6e71
Olivier Patry <olivier.patry@gmail.com>
2021-10-16 11:05:38 +0200

    Use try-with-resources
    See https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html

--- example/src/main/java/net/pempek/unicode/UnicodeBOMInputStreamUsage.java
+++ example/src/main/java/net/pempek/unicode/UnicodeBOMInputStreamUsage.java
@@ -4,10 +4,8 @@ package net.pempek.unicode;
 
 import java.io.*;
 
-public final class UnicodeBOMInputStreamUsage
-{
-  public static void main(final String[] args) throws Exception
-  {
+public final class UnicodeBOMInputStreamUsage {
+  public static void main(final String[] args) throws Exception {
     // here we're setting the output encoding to US-ASCII on purpose
     // would you choose UTF-8, Java would read it with U+FEFF, see
     // http://www.fileformat.info/info/unicode/char/feff/
@@ -15,36 +13,28 @@ public final class UnicodeBOMInputStreamUsage
     // with US-ASCII, the BOM is replaced by ?
     PrintStream out = new PrintStream(System.out, true, "US-ASCII");
 
-    InputStream is = UnicodeBOMInputStreamUsage.class.getResourceAsStream("/offending_bom.txt");
-    UnicodeBOMInputStream ubis = new UnicodeBOMInputStream(is);
+    try (InputStream is = UnicodeBOMInputStreamUsage.class.getResourceAsStream("/offending_bom.txt");
+         UnicodeBOMInputStream ubis = new UnicodeBOMInputStream(is);
+         InputStreamReader isr = new InputStreamReader(ubis);
+         BufferedReader br = new BufferedReader(isr)) {
 
-    out.println("Detected BOM: " + ubis.getBOM());
+      out.println("Detected BOM: " + ubis.getBOM());
 
-    out.print("Reading the content of the file without skipping the BOM: ");
-    InputStreamReader isr = new InputStreamReader(ubis);
-    BufferedReader br = new BufferedReader(isr);
+      out.print("Reading the content of the file without skipping the BOM: ");
 
-    out.println(br.readLine());
+      out.println(br.readLine());
+    }
 
-    br.close();
-    isr.close();
-    ubis.close();
-    is.close();
+    try (InputStream is = UnicodeBOMInputStreamUsage.class.getResourceAsStream("/offending_bom.txt");
+         UnicodeBOMInputStream ubis = new UnicodeBOMInputStream(is);
+         InputStreamReader isr = new InputStreamReader(ubis);
+         BufferedReader br = new BufferedReader(isr)) {
+      ubis.skipBOM();
 
-    is = UnicodeBOMInputStreamUsage.class.getResourceAsStream("/offending_bom.txt");
-    ubis = new UnicodeBOMInputStream(is);
-    ubis.skipBOM();
+      out.print("Reading the content of the file after skipping the BOM: ");
 
-    out.print("Reading the content of the file after skipping the BOM: ");
-    isr = new InputStreamReader(ubis);
-    br = new BufferedReader(isr);
-
-    out.println(br.readLine());
-
-    br.close();
-    isr.close();
-    ubis.close();
-    is.close();
+      out.println(br.readLine());
+    }
   }
 
 } // UnicodeBOMInputStreamUsage
